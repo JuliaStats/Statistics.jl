13de825ef303ca49ee3ffc50a1fad135cd6621b3
Benjamin Lungwitz <52384612+lungben@users.noreply.github.com>
2020-02-04 21:33:25 +0100

    fixed error in imports
    I did not intend to change the import part

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 # This file is a part of Julia. License is MIT: https://julialang.org/license
 
-using Statistics,
-using Test, Random, LinearAlgebra, SparseArrays
+using Statistics, Test, Random, LinearAlgebra, SparseArrays
 using Test: guardseed
 
 Random.seed!(123)
