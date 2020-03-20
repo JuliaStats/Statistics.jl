3290a2ba798e0ec1e76bc0f70b6648c4815b433f
kagalenko-m-b <Michael.Kagalenko@gmail.com>
2020-03-20 18:15:42 +0300

    Syntax change to avoid a spurious error

--- test/runtests.jl
+++ test/runtests.jl
@@ -139,9 +139,9 @@ end
         @test mean(x) == sum(x) / length(x)
     end
     @test mean(Number[1, 1.5, 2+3im]) === 1.5+1im # mixed-type array
-    @test @inferred mean(Int[]) === 0.0/0
-    @test @inferred mean(Float64[]) === 0.0/0
-    @test @inferred mean(Float32[]) === 0.0f0/0
+    @test (@inferred mean(Int[])) === 0.0/0
+    @test (@inferred mean(Float64[])) === 0.0/0
+    @test (@inferred mean(Float32[])) === 0.0f0/0
 end
 
 @testset "mean/median for ranges" begin
