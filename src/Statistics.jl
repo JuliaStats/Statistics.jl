6b8ab67c8b114601e7feeb244f09fc25e0fb3439
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-02-14 14:48:14 +0100

    Update src/Statistics.jl

--- src/Statistics.jl
+++ src/Statistics.jl
@@ -665,6 +665,10 @@ end
 
 # corzm (non-exported, with centered data)
 
+if !isdefined(Base, :nonmissingtype)
+    nonmissingtype(::Type{T}) where {T} = Core.Compiler.typesubtract(T, Missing)
+end
+
 corzm(x::AbstractVector{T}) where {T} =
     T === Missing ? missing : one(float(nonmissingtype(T)))
 function corzm(x::AbstractMatrix, vardim::Int=1)
