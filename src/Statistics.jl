9924bc75844444883ae5517a9964950ab2d1922e
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2022-03-30 14:35:13 +0100

    Change from quantile! to quantile
    Not sure what a meaningful way to work out the quantiles of a matrix inplace would look like, so remove the ! to make it clear it isn't inplace

--- src/Statistics.jl
+++ src/Statistics.jl
@@ -958,9 +958,6 @@ function quantile!(v::AbstractVector, p::Union{AbstractArray, Tuple{Vararg{Real}
     end
     return map(x->_quantile(v, x, alpha=alpha, beta=beta), p)
 end
-quantile!(arr::AbstractArray, p::Union{AbstractArray,Tuple{Vararg{Real}}};
-          sorted::Bool=false, alpha::Real=1.0, beta::Real=alpha) =
-    quantile!(vec(arr), p, sorted=sorted, alpha=alpha, beta=alpha)
 
 quantile!(v::AbstractVector, p::Real; sorted::Bool=false, alpha::Real=1., beta::Real=alpha) =
     _quantile(_quantilesort!(v, sorted, p, p), p, alpha=alpha, beta=beta)
@@ -1075,6 +1072,10 @@ quantile(itr, p; sorted::Bool=false, alpha::Real=1.0, beta::Real=alpha) =
 
 quantile(v::AbstractVector, p; sorted::Bool=false, alpha::Real=1.0, beta::Real=alpha) =
     quantile!(sorted ? v : Base.copymutable(v), p; sorted=sorted, alpha=alpha, beta=beta)
+
+quantile(arr::AbstractArray, p::Union{AbstractArray,Tuple{Vararg{Real}}};
+    sorted::Bool=false, alpha::Real=1.0, beta::Real=alpha) =
+    quantile!(vec(arr), p, sorted=sorted, alpha=alpha, beta=alpha)
 
 
 ##### SparseArrays optimizations #####
